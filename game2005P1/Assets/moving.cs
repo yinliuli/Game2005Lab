db2b603b7fdf8f509cabbbc36c3e7860a12449a8
yinliuli <dengzhuoxi666@gmail.com>
2024-09-12 13:47:26 -0400

    Ex1 Update

---
+++ game2005P1/Assets/moving.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class moving : MonoBehaviour
+{
+
+    Vector3 begin = new Vector3 (0, 0, 0);
+    public float speed = 1/2;
+    public float frequency = 1/6;
+    public float amplitude = 1/3;
+
+    void Start()
+    {
+
+        transform.position = (begin);        
+
+    }
+
+    void Update()
+    {
+        float gt = Time.time;
+        float dt = Time.fixedDeltaTime;
+        float objectX = -Mathf.Sin(gt * amplitude) * amplitude * frequency * dt;
+        float objectY = Mathf.Cos(gt * amplitude) * amplitude * frequency * dt;
+
+        transform.position = new Vector3(transform.position.x + objectX, transform.position.y + objectY, transform.position.z);
+
+    }
+
+
+
+}
