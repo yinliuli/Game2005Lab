39d531ebfe739bf100a85324a03f78d51401d3f9
Zhuoxi Deng(101499352) <148092207+yinliuli@users.noreply.github.com>
2024-09-15 16:15:43 -0400

    Update moving.cs

--- game2005P1/Assets/moving.cs
+++ game2005P1/Assets/moving.cs
@@ -7,9 +7,8 @@ public class moving : MonoBehaviour
 {
 
     Vector3 begin = new Vector3 (0, 0, 0);
-    public float speed;
-    public float frequency;
-    public float amplitude;
+    public float frequency = 1;
+    public float amplitude = 1;
 
     void Start()
     {
@@ -23,7 +22,7 @@ public class moving : MonoBehaviour
         float gt = Time.time;
         float dt = Time.fixedDeltaTime;
         float objectX = -Mathf.Sin(gt * amplitude) * amplitude * frequency * dt;
-        float objectY = Mathf.Cos(gt * amplitude) * amplitude * frequency * dt;
+        float objectY = -Mathf.Cos(gt * amplitude) * amplitude * frequency * dt;
 
         transform.position = new Vector3(transform.position.x + objectX, transform.position.y + objectY, transform.position.z);
 
