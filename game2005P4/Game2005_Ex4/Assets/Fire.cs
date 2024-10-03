83ffb2fcf99554fc2a1f53d921139ca0c3444090
yinliuli <dengzhuoxi666@gmail.com>
2024-10-03 11:44:16 -0400

    update

---
+++ game2005P4/Game2005_Ex4/Assets/Fire.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public float speed;
+    public float angle;
+    bool StartMoving = false;
+    public float dt = 0.02f;
+    public Vector3 AccGravity = new Vector3(0, -10, 0);
+    public Vector3 velocity;
+
+
+    void Start()
+    {
+        transform.position = Vector3.zero;
+        StartMoving = false;
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float angleInRadians = angle * Mathf.Deg2Rad;
+
+        Vector3 velocity = new Vector3(Mathf.Cos(angleInRadians) * speed, Mathf.Sin(angleInRadians) * speed, 0);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position = Vector3.zero;
+            StartMoving = (true);
+            Debug.Log("Start");
+            AccGravity = new Vector3(0, -10, 0);
+        }
+
+        if (StartMoving)
+        {
+            Vector3 PreviousPosition = transform.position;
+            AccGravity += AccGravity * dt;
+            transform.position = transform.position + velocity * dt + AccGravity * dt;
+
+            Debug.DrawLine(PreviousPosition, transform.position, Color.red, 10);
+
+        }
+
+    }
+
+}
