01a3989a620147f51b481971a700d57d6f55da85
yinliuli <dengzhuoxi666@gmail.com>
2024-10-06 18:43:12 -0400

    update Ex4

--- game2005P4/Game2005_Ex4/Assets/Fire.cs
+++ game2005P4/Game2005_Ex4/Assets/Fire.cs
@@ -1,51 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
     public float speed;
     public float angle;
-    bool StartMoving = false;
     public float dt = 0.02f;
     public Vector3 AccGravity = new Vector3(0, -10, 0);
-    public Vector3 velocity;
+    public Vector3 initialVelocity;
+    public float Drag = 0.02f;
+    public GameObject ballPrefab;
 
 
     void Start()
     {
         transform.position = Vector3.zero;
-        StartMoving = false;
-
-
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
         float angleInRadians = angle * Mathf.Deg2Rad;
+        initialVelocity.x = Mathf.Cos(angleInRadians) * speed;
+        initialVelocity.y = Mathf.Sin(angleInRadians) * speed;
+        initialVelocity.z = 0.0f;
 
-        Vector3 velocity = new Vector3(Mathf.Cos(angleInRadians) * speed, Mathf.Sin(angleInRadians) * speed, 0);
-
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position = Vector3.zero;
-            StartMoving = (true);
-            Debug.Log("Start");
-            AccGravity = new Vector3(0, -10, 0);
-        }
-
-        if (StartMoving)
-        {
-            Vector3 PreviousPosition = transform.position;
-            AccGravity += AccGravity * dt;
-            transform.position = transform.position + velocity * dt + AccGravity * dt;
-
-            Debug.DrawLine(PreviousPosition, transform.position, Color.red, 10);
-
+            CreateBall();
         }
 
     }
 
+    void CreateBall()
+    {
+        GameObject ball = Instantiate(ballPrefab, transform.position, transform.rotation);
+        BallMotion motion = ball.AddComponent<BallMotion>();
+
+        motion.Initialize(initialVelocity, AccGravity, Drag);
+    }
+
+}
+
+public class BallMotion : MonoBehaviour
+{
+    public Vector3 velocity;
+    private Vector3 gravity;
+    private float dragCoefficient;
+    public void Initialize(Vector3 initialVelocity, Vector3 gravity, float dragCoefficient)
+    {
+        this.velocity = initialVelocity;
+        this.gravity = gravity;
+        this.dragCoefficient = dragCoefficient;
+    }
+
+    void FixedUpdate()
+    {
+        float deltaTime = Time.deltaTime;
+        Vector3 PreviousPosition = transform.position;
+
+        Vector3 drag = -dragCoefficient * velocity;
+
+        velocity += (gravity + drag) * deltaTime;
+
+        transform.position += velocity * deltaTime;
+
+        Debug.DrawLine(PreviousPosition, transform.position, Color.red, 15);
+
+    }
 }
