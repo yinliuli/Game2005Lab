4a018c7f8a2eec1286092890ebf3378a91640bb6
yinliuli <dengzhuoxi666@gmail.com>
2024-10-13 22:53:14 -0400

    Update collisions

--- game2005P5/Game2005_Ex5/Assets/Fire.cs
+++ game2005P5/Game2005_Ex5/Assets/Fire.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class Fire : MonoBehaviour
     public Vector3 initialVelocity;
     public float Drag = 0.02f;
     public GameObject ballPrefab;
+    public float radius = 0.5f;
+    public List<BallMotion> balls = new List<BallMotion>();
+    private int BallsCounts = 0;
 
 
     void Start()
@@ -17,17 +22,18 @@ public class Fire : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float angleInRadians = angle * Mathf.Deg2Rad;
         initialVelocity.x = Mathf.Cos(angleInRadians) * speed;
         initialVelocity.y = Mathf.Sin(angleInRadians) * speed;
         initialVelocity.z = 0.0f;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
             CreateBall();
         }
+        CheckForCollisions();
 
     }
 
@@ -36,22 +42,69 @@ public class Fire : MonoBehaviour
         GameObject ball = Instantiate(ballPrefab, transform.position, transform.rotation);
         BallMotion motion = ball.AddComponent<BallMotion>();
 
-        motion.Initialize(initialVelocity, AccGravity, Drag);
+        motion.Initialize(initialVelocity, AccGravity, Drag, radius);
+        BallsCounts++;
+        ball.name = "Ball" + BallsCounts;
+        balls.Add(motion);
+
+    }
+    void CheckForCollisions()
+    {
+        for (int i = 0; i < balls.Count; i++)
+        {
+            for (int j = i + 1; j < balls.Count; j++)
+            {
+                float distance = Vector3.Distance(balls[i].transform.position, balls[j].transform.position);
+                if (distance <= balls[i].radius + balls[j].radius)
+                {
+                    balls[i].SetColor(Color.red);
+                    balls[j].SetColor(Color.red);
+                    Debug.Log(balls[i].name + " is collision with " + balls[j].name);
+                }
+                else
+                {
+                    balls[i].SetColor(Color.white);
+                    balls[j].SetColor(Color.white);
+
+                }
+            }
+        }
     }
 
+
+
+
+
 }
+
+
+
 
 public class BallMotion : MonoBehaviour
 {
     public Vector3 velocity;
     private Vector3 gravity;
     private float dragCoefficient;
-    private bool UnderZero  = false;
-    public void Initialize(Vector3 initialVelocity, Vector3 gravity, float dragCoefficient)
+    public float radius = 1;
+    private Renderer ballRenderer;
+
+
+    public void Initialize(Vector3 initialVelocity, Vector3 gravity, float dragCoefficient, float Radius)
     {
         this.velocity = initialVelocity;
         this.gravity = gravity;
         this.dragCoefficient = dragCoefficient;
+        radius = Radius;
+        transform.localScale = new Vector3(radius, radius, radius) * 2;
+
+        ballRenderer = GetComponent<Renderer>();
+        if (ballRenderer == null)
+        {
+            ballRenderer = gameObject.AddComponent<MeshRenderer>();
+        }
+
+        ballRenderer.material.color = Color.white;
+
     }
 
     void FixedUpdate()
@@ -59,19 +112,21 @@ public class BallMotion : MonoBehaviour
         float deltaTime = Time.deltaTime;
         Vector3 PreviousPosition = transform.position;
 
-        Vector3 drag = -dragCoefficient * velocity;
+        Vector3 drag = -dragCoefficient * velocity.sqrMagnitude * velocity.normalized;
 
         velocity += (gravity + drag) * deltaTime;
 
         transform.position += velocity * deltaTime;
 
-        Debug.DrawLine(PreviousPosition, transform.position, Color.red, 15);
+        Debug.DrawLine(PreviousPosition, transform.position, Color.red, 30);
 
-        if (!UnderZero && transform.position.y <= 0)
-        {
-            Debug.Log(transform.position.x);
-            UnderZero = true;
-        }
 
     }
+    public void SetColor(Color color)
+    {
+        ballRenderer.material.color = color;
+    }
+
+
+
 }
